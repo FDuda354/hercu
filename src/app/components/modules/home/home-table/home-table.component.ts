3c66593e38df38179723626ff25594ece6a013b0
FDuda354 <92389794+w60846@users.noreply.github.com>
2024-05-07 00:28:18 +0200

    convert transacion shor table

--- src/app/components/modules/home/home-table/home-table.component.ts
+++ src/app/components/modules/home/home-table/home-table.component.ts
@@ -1,15 +1,25 @@
 import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { Transaction } from '../../../common/models/transaction';
 import { DebtStatus } from '../../../common/models/debt-dto';
+import {TransactionService} from "../../../../services/transaction.service";
+import {MessageService} from "primeng/api";
+import {Page} from "../../../common/models/page";
 
 @Component({
   selector: 'app-home-table',
   templateUrl: './home-table.component.html',
-  styleUrls: ['./home-table.component.scss']
+  styleUrls: ['./home-table.component.scss'],
+  providers: [MessageService]
 })
 export class HomeTableComponent implements OnInit {
-  @Input() transactions!: Transaction[];
+  transactions: Transaction[] = [];
   isMobileVisible = false;
+
+  constructor(
+    private transactionService: TransactionService,
+    private messageService: MessageService,
+  ) {
+  }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
@@ -18,6 +28,31 @@ export class HomeTableComponent implements OnInit {
 
   ngOnInit(): void {
     this.isMobileVisible = window.innerWidth <= 1050;
+    this.loadTransaction()
+
+  }
+
+  private loadTransaction() {
+    this.transactionService.getTransactionsByCustomerId(0, 5).subscribe({
+      next: (page: Page<Transaction>) => {
+        this.transactions = page.content;
+      },
+      error: error => {
+        console.error('Error loading customers', error);
+        this.showIncorrectLogin()
+      }
+    });
+  }
+
+  showIncorrectLogin() {
+    this.messageService.add({
+      key: 'tr',
+      severity: 'error',
+      summary: 'Błąd Servera',
+      detail: 'Nie udało się pobrać ostatnich transakcji',
+      life: 10000
+
+    });
   }
 
   getDebtStatus(status: DebtStatus): string {
--- src/app/components/modules/home/home.component.ts
+++ src/app/components/modules/home/home.component.ts
@@ -1,50 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Transaction } from '../../common/models/transaction';
-import { TransactionService } from '../../../services/transaction.service';
-import { Page } from '../../common/models/page';
-import { MessageService } from 'primeng/api';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  providers: [MessageService]
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  transactions: Transaction[] = [];
-
-
-  constructor(
-    private transactionService: TransactionService,
-    private messageService: MessageService,
-  ) {
-  }
-
-  ngOnInit(): void {
-    this.loadTransaction()
-  }
-
-  private loadTransaction() {
-    this.transactionService.getTransactionsByCustomerId(0, 5).subscribe({
-      next: (page: Page<Transaction>) => {
-        this.transactions = page.content;
-      },
-      error: error => {
-        console.error('Error loading customers', error);
-        this.showIncorrectLogin()
-      }
-    });
-  }
-
-  showIncorrectLogin() {
-    this.messageService.add({
-      key: 'tr',
-      severity: 'error',
-      summary: 'Błąd Servera',
-      detail: 'Nie udało się pobrać ostatnich transakcji',
-      life: 10000
-
-    });
-  }
+export class HomeComponent {
 
 }
